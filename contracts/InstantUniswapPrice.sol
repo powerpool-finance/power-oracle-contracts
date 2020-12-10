5400d0e0cc8c3cf43ef2ba075d105177eec7f204
defi-dev <defi.dev@protonmail.com>
2020-12-10 18:39:44 +0200

    remove experimental decoder

--- contracts/InstantUniswapPrice.sol
+++ contracts/InstantUniswapPrice.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.6.12;
-pragma experimental ABIEncoderV2;
 
 import "./Uniswap/UniswapV2Library.sol";
 import "./interfaces/IUniswapV2Pair.sol";
