2e9d49336f46682ab1e7d94da8d4c296edfc5e08
defi-dev <defi.dev@protonmail.com>
2021-02-08 01:10:37 +0300

    update deploy scripts

--- tasks/deployTestnet.js
+++ tasks/deployTestnet.js
@@ -18,16 +18,11 @@ task('deploy-testnet', 'Deploys testnet contracts')
     const SLASHER_REWARD_PCT = ether(15);
     const RESERVOIR_REWARD_PCT = ether(5);
     const MockCVP = artifacts.require('MockCVP');
-    const BPool = artifacts.require('BPoolInterface');
     const OWNER = '0xe7F2f6bb028E2c01C2C34e01BFFe5f534E7f1901';
     // The same as deployer
     // const RESERVOIR = '0x0A243E1867F682D6c6e7b446a43800977ff58024';
     const RESERVOIR = '0xfE2AB24d7855093E3d90aa298a676FEDA9fab7a0';
     const POOL_ADDRESS = '0x56038007b8de3CDbFd19da17909DBDc2bB5c0c45';
-
-    const bpool = await BPool.at(POOL_ADDRESS);
-    console.log(bpool.address);
-    console.log('totalSupply', await bpool.contract.methods.totalSupply().call());
 
     const PowerPoke = artifacts.require('PowerPoke');
     const PowerPokeStaking = artifacts.require('PowerPokeStaking');
@@ -75,20 +70,35 @@ task('deploy-testnet', 'Deploys testnet contracts')
 
     console.log('>>> Deploying PowerOracle...');
     let oracle;
+    let tokensSymbols = [];
     if(POOL_ADDRESS) {
       const IUniswapV2Factory = artifacts.require('IUniswapV2Factory');
       const IERC20 = artifacts.require('IERC20Detailed');
+      const BPool = artifacts.require('BPoolInterface');
+      const bpool = await BPool.at(POOL_ADDRESS);
       const uniswapFactory = await IUniswapV2Factory.at('0x4b2387242d2E1415A7Ce9ee584082d4B9d796061');
       const wethAddress = '0xed0F538448Cc27B1deF57feAc43201C79e6bDCf7';
       const usdcAddress = '0xdbb2b2550bd5f6091756ed9bb674388283d42bf4';
       const tokensConfig = [
         {cToken: wethAddress, underlying: wethAddress, symbolHash: keccak256('ETH'), baseUnit: ether(1), priceSource: 2, fixedPrice: 0, uniswapMarket: await uniswapFactory.getPair(usdcAddress, wethAddress), isUniswapReversed: true},
       ];
-      const poolTokens = await bpool.getCurrentTokens();
+      tokensSymbols.push('ETH');
+      const poolTokens = [
+        '0xB771f325877b18977A44e2A26c9B202E3a2F4E80',
+        '0x07e081Bcc6Cd8B7Cb68D4B9dB36B46Dd9663E8b4',
+        '0x8D27d7cb7569467FA1d2f860c90e5C0D79dC10FD',
+        '0xD6bf1F32da9F194e3b5aaf40c056F5079317bE89',
+        '0xB76C1c2C49ccB707De99DbE207dd8eAEDF9aA751',
+        '0x73c82a86866699E6ecE9cE5b3113F4B63A93AF87',
+        '0xb0b4f73240Ed3907e2550F35397D266E4E86A3a8',
+        '0xED98CaEe836eA4dABbD5FDD9b2c34AB26a47FD41'
+      ]; // = pool.getCurrentTokens();
 
       await pIteration.forEach(poolTokens, async (tokenAddr) => {
         const token = await IERC20.at(tokenAddr);
-        tokensConfig.push({cToken: tokenAddr, underlying: tokenAddr, symbolHash: keccak256(await token.symbol()), baseUnit: ether(1), priceSource: 2, fixedPrice: 0, uniswapMarket: await uniswapFactory.getPair(tokenAddr, wethAddress), isUniswapReversed: false})
+        const symbol = await token.symbol();
+        tokensSymbols.push(symbol);
+        tokensConfig.push({cToken: tokenAddr, underlying: tokenAddr, symbolHash: keccak256(symbol), baseUnit: ether(1), priceSource: 2, fixedPrice: 0, uniswapMarket: await uniswapFactory.getPair(tokenAddr, wethAddress), isUniswapReversed: false})
       })
       oracle = await deployProxied(
         PowerOracle,
@@ -97,9 +107,11 @@ task('deploy-testnet', 'Deploys testnet contracts')
         { proxyAdminOwner: OWNER }
       );
     } else {
+      const tokenConfigs = await getTokenConfigs(cvpToken.address);
+      tokensSymbols = ['ETH', 'DAI', 'REP', 'BTC', 'CVP'];
       oracle = await deployProxied(
         PowerOracle,
-        [cvpToken.address, ANCHOR_PERIOD, await getTokenConfigs(cvpToken.address)],
+        [cvpToken.address, ANCHOR_PERIOD, tokenConfigs],
         [OWNER, powerPoke.address],
         { proxyAdminOwner: OWNER }
       );
@@ -122,7 +134,7 @@ task('deploy-testnet', 'Deploys testnet contracts')
     await cvpToken.approve(oracle.address, 10000);
 
     console.log('>>> Making the initial poke');
-    await oracle.poke(['ETH', 'DAI', 'REP', 'BTC', 'CVP']);
+    await oracle.poke(tokensSymbols);
 
     console.log('Done');
   });
